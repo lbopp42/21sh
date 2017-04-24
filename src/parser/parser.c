472c08c6576ca6249d4177e7f18ec6191ca624ab
Lucas BOPP <lbopp@student.42.fr>
2017-04-24 14:11:23 +0200

    Parser : Simple_cmd part is done, I'm testing it

---
+++ src/parser/parser.c
@@ -0,0 +1,153 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/24 12:10:28 by lbopp             #+#    #+#             */
+/*   Updated: 2017/04/24 13:59:28 by lbopp            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lsh.h"
+
+int	isio_file(t_token *tok_lst, int nb_tok, int *max)
+{
+	(void)nb_tok;
+	if (tok_lst->type == LESS && tok_lst->next->type == WORD)
+	{
+		if (tok_lst->next->i > *max)
+			*max = tok_lst->next->i;
+			return (1);
+	}
+	else if (tok_lst->type == LESSAND && tok_lst->next->type == WORD)
+	{
+		if (tok_lst->next->i > *max)
+			*max = tok_lst->next->i;
+			return (1);
+	}
+	else if (tok_lst->type == GREAT && tok_lst->next->type == WORD)
+	{
+		if (tok_lst->next->i > *max)
+			*max = tok_lst->next->i;
+			return (1);
+	}
+	else if (tok_lst->type == GREATAND && tok_lst->next->type == WORD)
+	{
+		if (tok_lst->next->i > *max)
+			*max = tok_lst->next->i;
+			return (1);
+	}
+	else if (tok_lst->type == DGREAT && tok_lst->next->type == WORD)
+	{
+		if (tok_lst->next->i > *max)
+			*max = tok_lst->next->i;
+			return (1);
+	}
+	return (0);
+}
+
+/*
+**	nb_tok - tok_lst->i >= 2:
+**	This is use to check if we have enought token to use the function
+*/
+
+int	isio_redirect(t_token *tok_lst, int nb_tok, int *max)
+{
+	if (nb_tok - tok_lst->i >= 2 && isio_file(tok_lst, nb_tok, max))
+	{
+		if (tok_lst->i > *max)
+			*max = tok_lst->i;
+			return (1);
+	}
+	else if (tok_lst->type == IO_NUMBER && tok_lst->next &&
+			isio_file(tok_lst->next, nb_tok, max))
+	{
+		if (tok_lst->i > *max)
+			*max = tok_lst->i;
+			return (1);
+	}
+	else if (tok_lst->type == DLESS && tok_lst->next &&
+			tok_lst->next->type == WORD)
+	{
+		if (tok_lst->i > *max)
+			*max = tok_lst->i;
+			return (1);
+	}
+	else if (tok_lst->type == IO_NUMBER && tok_lst->next &&
+			tok_lst->next->type == DLESS && tok_lst->next->next &&
+			tok_lst->next->next->type == WORD)
+	{
+		if (tok_lst->i > *max)
+			*max = tok_lst->i;
+			return (1);
+	}
+	return (0);
+}
+
+/*
+**	The static [i] is here to avoid an infinity loop
+**	2 * i - (i - 1) : this is to avoid an infinity loop too
+**	-> It will begin with 2 and increase 1 by 1
+*/
+
+int	iscmd_prefix(t_token *tok_lst, int nb_tok, int *max)
+{
+	static int i = -1;
+
+	i++;
+	if (nb_tok - tok_lst->i >= 2 * i - (i - 1) && iscmd_prefix(tok_lst, nb_tok, max) &&
+			tok_lst->next && isio_redirect(tok_lst->next, nb_tok, max))
+	{
+		if (tok_lst->i > *max)
+			*max = tok_lst->i;
+			return (1);
+	}
+	else if (isio_redirect(tok_lst, nb_tok, max))
+	{
+		if (tok_lst->i > *max)
+			*max = tok_lst->i;
+			return (1);
+	}
+	return (0);
+}
+
+int	is_simplecmd(t_token *tok_lst, int nb_tok, int *max)
+{
+	if (iscmd_prefix(tok_lst, nb_tok, max) && tok_lst->next &&
+			tok_lst->next->type == WORD && tok_lst->next->next &&
+			iscmd_prefix(tok_lst->next->next, nb_tok, max))
+	{
+		if (tok_lst->i > *max)
+			*max = tok_lst->i;
+			return (1);
+	}
+	else if (iscmd_prefix(tok_lst, nb_tok, max) && tok_lst->next->type &&
+			tok_lst->next->type == WORD)
+	{
+		if (tok_lst->i > *max)
+			*max = tok_lst->i;
+			return (1);
+	}
+	else if (iscmd_prefix(tok_lst, nb_tok, max))
+	{
+		if (tok_lst->i > *max)
+			*max = tok_lst->i;
+			return (1);
+	}
+	else if (tok_lst->type == WORD && tok_lst->next &&
+			iscmd_prefix(tok_lst->next, nb_tok, max))
+	{
+		if (tok_lst->i > *max)
+			*max = tok_lst->i;
+			return (1);
+	}
+	else if (tok_lst->type == WORD)
+	{
+		if (tok_lst->i > *max)
+			*max = tok_lst->i;
+			return (1);
+	}
+	return (0);
+}
---
+++ src/parser/testparser.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testparser.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/24 13:49:21 by lbopp             #+#    #+#             */
+/*   Updated: 2017/04/24 14:09:54 by lbopp            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lsh.h"
+
+int	is_simplecmd(t_token *tok_lst, int nb_tok, int *max);
+
+int	test1(void)
+{
+	int	max;
+	int	nb_tok;
+	t_token	*tok_lst;
+
+	max = 0;
+	nb_tok = 1;
+	tok_lst= (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->i = 0;
+	tok_lst->type = WORD;
+	tok_lst->content = ft_strdup("ls");
+	tok_lst->next = NULL;
+	if (is_simplecmd(tok_lst, nb_tok, &max) && max == nb_tok - 1)
+		return (1);
+	else
+		return (0);
+}
+
+int	test2(void)
+{
+	int	max;
+	int	nb_tok;
+	t_token	*tok_lst;
+
+	max = 0;
+	nb_tok = 2;
+	tok_lst= (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->i = 0;
+	tok_lst->type = WORD;
+	tok_lst->content = ft_strdup("ls");
+	tok_lst->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->i = 1;
+	tok_lst->next->type = GREAT;
+	tok_lst->next->content = ft_strdup(">");
+	tok_lst->next->next = NULL;
+	if (is_simplecmd(tok_lst, nb_tok, &max) && max == nb_tok - 1)
+		return (1);
+	else
+		return (0);
+}
+
+int	test3(void)
+{
+	int	max;
+	int	nb_tok;
+	t_token	*tok_lst;
+
+	max = 0;
+	nb_tok = 2;
+	tok_lst= (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->i = 0;
+	tok_lst->type = GREAT;
+	tok_lst->content = ft_strdup(">");
+	tok_lst->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->i = 1;
+	tok_lst->next->type = WORD;
+	tok_lst->next->content = ft_strdup("ls");
+	tok_lst->next->next = NULL;
+	if (is_simplecmd(tok_lst, nb_tok, &max) && max == nb_tok - 1)
+		return (1);
+	else
+		return (0);
+}
+
+void	launch_test_1to25(void)
+{
+	if (test1())
+		printf("\033[32m[OK] \033[0m[ls]\n");
+	else
+		printf("\033[31m[FAIL] \033[0m[ls]\n");
+	if (!test2())
+		printf("\033[32m[OK] \033[0m[ls >]\n");
+	else
+		printf("\033[31m[FAIL] \033[0m[ls >]\n");
+	if (test3())
+		printf("\033[32m[OK] \033[0m[> ls]\n");
+	else
+		printf("\033[31m[FAIL] \033[0m[> ls]\n");
+}
+
+int	main(void)
+{
+	printf("\033[36m=========  Testing parser of lsh  =========\n\n\033[0m");
+	launch_test_1to25();
+}
