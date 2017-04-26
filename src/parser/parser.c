430e15cc86843b041fc1632f2545af44d0aa8b61
Lucas BOPP <lbopp@student.42.fr>
2017-04-26 16:40:02 +0200

    I think it is the good time for the parser : issimplecmd is ok ✅

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,13 +6,13 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/24 12:10:28 by lbopp             #+#    #+#             */
-/*   Updated: 2017/04/26 15:22:03 by lbopp            ###   ########.fr       */
+/*   Updated: 2017/04/26 16:38:42 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lsh.h"
 
-int	isio_file(t_token *tok_lst, int nb_tok, int *max, int mv)
+int	isio_file(t_token *tok_lst, int nb_tok, int mv)
 {
 	int	tmp;
 
@@ -25,38 +25,17 @@ int	isio_file(t_token *tok_lst, int nb_tok, int *max, int mv)
 			return (0);
 		tmp--;
 	}
-	tmp = mv;
 	(void)nb_tok;
 	if (tok_lst->type == LESS && tok_lst->next->type == WORD)
-	{
-		if (mv + 2 - 1 > *max)
-			*max = mv + 2 - 1;
 			return (mv + 2);
-	}
 	else if (tok_lst->type == LESSAND && tok_lst->next->type == WORD)
-	{
-		if (mv + 2 - 1 > *max)
-			*max = mv + 2 - 1;
 			return (mv + 2);
-	}
 	else if (tok_lst->type == GREAT && tok_lst->next->type == WORD)
-	{
-		if (mv + 2 - 1 > *max)
-			*max = mv + 2 - 1;
 		return (mv + 2);
-	}
 	else if (tok_lst->type == GREATAND && tok_lst->next->type == WORD)
-	{
-		if (mv + 2 - 1 > *max)
-			*max = mv + 2 - 1;
 			return (mv + 2);
-	}
 	else if (tok_lst->type == DGREAT && tok_lst->next->type == WORD)
-	{
-		if (mv + 2 - 1 > *max)
-			*max = mv + 2;
 			return (mv + 2);
-	}
 	return (0);
 }
 
@@ -65,7 +44,7 @@ int	isio_file(t_token *tok_lst, int nb_tok, int *max, int mv)
 **	This is use to check if we have enought token to use the function
 */
 
-int	isio_redirect(t_token *tok_lst, int nb_tok, int *max, int mv)
+int	isio_redirect(t_token *tok_lst, int nb_tok, int mv)
 {
 	int	tmp;
 
@@ -78,34 +57,18 @@ int	isio_redirect(t_token *tok_lst, int nb_tok, int *max, int mv)
 			return (0);
 		tmp--;
 	}
-	if (nb_tok - tmp >= 2 && (tmp = isio_file(tok_lst, nb_tok, max, 0)))
-	{
-		if (mv + tmp - 1 > *max)
-			*max = mv + tmp - 1;
+	if (nb_tok - tmp >= 2 && (tmp = isio_file(tok_lst, nb_tok, 0)))
 		return (mv + tmp);
-	}
 	else if (tok_lst->type == IO_NUMBER &&
-			(mv = isio_file(tok_lst, nb_tok, max, 1)))
-	{
-		if (mv - 1 > *max)
-			*max = mv - 1;
+			(mv = isio_file(tok_lst, nb_tok, 1)))
 			return (mv + tmp);
-	}
 	else if (tok_lst->type == DLESS && tok_lst->next &&
 			tok_lst->next->type == WORD)
-	{
-		if (2 + mv - 1 > *max)
-			*max = 2 + mv - 1;
 			return (2 + mv);
-	}
 	else if (tok_lst->type == IO_NUMBER && tok_lst->next &&
 			tok_lst->next->type == DLESS && tok_lst->next->next &&
 			tok_lst->next->next->type == WORD)
-	{
-		if (3 + mv - 1 > *max)
-			*max = 3 + mv - 1;
 			return (3 + mv);
-	}
 	return (0);
 }
 
@@ -116,9 +79,37 @@ int	isio_redirect(t_token *tok_lst, int nb_tok, int *max, int mv)
 **	-> It will begin with 3 and increase 2 by 2
 */
 
-int	iscmd_prefix(t_token *tok_lst, int nb_tok, int *max, int mv)
+int	iscmd_prefix(t_token *tok_lst, int nb_tok, int mv, int mode)
 {
 	static int i = 0;
+	int	tmp;
+
+	tmp = mv;
+	if (mode == 1)
+		i = 0;
+	while (tmp > 0)
+	{
+		if (tok_lst->next)
+			tok_lst = tok_lst->next;
+		else
+			return (0);
+		tmp--;
+	}
+	i++;
+	if (nb_tok - mv >= 4 * i - (i - 1) && (mv = iscmd_prefix(tok_lst, nb_tok, 0, 2)) &&
+			tok_lst->next && (mv = isio_redirect(tok_lst, nb_tok, mv)))
+	{
+		return (mv);
+	}
+	else if ((mv = isio_redirect(tok_lst, nb_tok, 0)))
+	{
+		return (mv);
+	}
+	return (0);
+}
+
+int	is_word(t_token *tok_lst, int mv)
+{
 	int	tmp;
 
 	tmp = mv;
@@ -130,20 +121,41 @@ int	iscmd_prefix(t_token *tok_lst, int nb_tok, int *max, int mv)
 			return (0);
 		tmp--;
 	}
-	i++;
-	if (nb_tok - tmp >= 4 * i - (i - 1) && (tmp = iscmd_prefix(tok_lst, nb_tok, max, 0)) &&
-			tok_lst->next && (mv = isio_redirect(tok_lst, nb_tok, max, tmp)))
+	if (tok_lst->type == WORD)
+		return (mv + 1);
+	else
+		return (0);
+}
+
+int	is_simplecmd(t_token *tok_lst, int nb_tok, int mv)
+{
+	int	tmp;
+
+	tmp = mv;
+	while (tmp > 0)
 	{
-		if (mv - 1 > *max)
-			*max = mv - 1;
-			return (mv);
+		if (tok_lst->next)
+			tok_lst = tok_lst->next;
+		else
+			return (0);
+		tmp--;
 	}
-	else if ((mv = isio_redirect(tok_lst, nb_tok, max, tmp)))
+	if ((mv = iscmd_prefix(tok_lst, nb_tok, 0, 1)) && (mv = is_word(tok_lst, mv)) &&
+			(mv = iscmd_prefix(tok_lst, nb_tok, mv, 1)))
 	{
-		if (mv - 1 > *max)
-			*max = mv - 1;
-			return (mv);
+		return (mv);
 	}
+	else if ((mv = iscmd_prefix(tok_lst, nb_tok, 0, 1)) && (mv = is_word(tok_lst, mv)))
+	{
+		return (mv);
+	}
+	else if ((mv = iscmd_prefix(tok_lst, nb_tok, 0, 1)))
+		return (mv);
+	else if (tok_lst->type == WORD && tok_lst->next &&
+			(mv = iscmd_prefix(tok_lst->next, nb_tok, 1, 1)))
+		return (mv + 1);
+	else if (tok_lst->type == WORD)
+		return (1);
 	return (0);
 }
 
@@ -151,72 +163,26 @@ int	main(void)
 {
 	t_token	*tok_lst;
 	int	nb_tok;
-	int	max;
 	int	mv;
 
-	max = 0;
 	mv = 0;
-	nb_tok = 4;
+	nb_tok = 3;
 	tok_lst = (t_token*)ft_memalloc(sizeof(t_token));
 	tok_lst->i = 0;
-	tok_lst->type = DGREAT;
-	tok_lst->content = ft_strdup(">>");
+	tok_lst->type = IO_NUMBER;
+	tok_lst->content = ft_strdup("1");
 	tok_lst->next = (t_token*)ft_memalloc(sizeof(t_token));
 	tok_lst->next->i = 1;
-	tok_lst->next->type = WORD;
-	tok_lst->next->content = ft_strdup("ls");
+	tok_lst->next->type = GREAT;
+	tok_lst->next->content = ft_strdup(">");
 	tok_lst->next->next = (t_token*)ft_memalloc(sizeof(t_token));
 	tok_lst->next->next->i = 2;
-	tok_lst->next->next->type = DLESS;
-	tok_lst->next->next->content = ft_strdup("<<");
-	tok_lst->next->next->next = (t_token*)ft_memalloc(sizeof(t_token));//NULL;
+	tok_lst->next->next->type = WORD;
+	tok_lst->next->next->content = ft_strdup("lol");
+	tok_lst->next->next->next = NULL;/*(t_token*)ft_memalloc(sizeof(t_token));//NULL;
 	tok_lst->next->next->next->i = 3;
 	tok_lst->next->next->next->type = WORD;
 	tok_lst->next->next->next->content = ft_strdup("lol");
-	tok_lst->next->next->next->next = NULL;
-	printf("Result = %d et max = %d\n", iscmd_prefix(tok_lst, nb_tok, &max, mv), max);
+	tok_lst->next->next->next->next = NULL;*/
+	printf("Result = %d\n", is_simplecmd(tok_lst, nb_tok, mv));
 }
-
-/*int	is_simplecmd(t_token *tok_lst, int nb_tok, int *max)
-{
-	int	mv;
-
-	mv = 0;
-	if ((mv = iscmd_prefix(tok_lst, nb_tok, max, mv)) && tok_lst->next &&
-			tok_lst->next->type == WORD && tok_lst->next->next &&
-			(mv = iscmd_prefix(tok_lst, nb_tok, max, mv + 1)))
-	{
-		if (mv + 1 - 1 > *max)
-			*max = mv + 1 - 1;
-			return (mv + 1);
-	}
-	else if (printf("ON PASSE\n") && (mv = iscmd_prefix(tok_lst, nb_tok, max, 0)) && tok_lst->next->type &&
-			tok_lst->next->type == WORD)
-	{
-		printf("C'est bien la, et i = %d\n", tok_lst->next->i);
-		if (mv + 1 - 1 > *max)
-			*max = mv + 1 - 1;
-		printf("max = %d\n", *max);
-			return (mv + 1);
-	}
-	else if ((mv = iscmd_prefix(tok_lst, nb_tok, max, 0)))
-	{
-		if (mv - 1 > *max)
-			*max = mv - 1;
-			return (mv);
-	}
-	else if (tok_lst->type == WORD && tok_lst->next &&
-			(mv = iscmd_prefix(tok_lst->next, nb_tok, max, 0)))
-	{
-		if (mv + 1 - 1 > *max)
-			*max = mv + 1 - 1;
-			return (mv + 1);
-	}
-	else if (tok_lst->type == WORD)
-	{
-		if (1 - 1 > *max)
-			*max = 1 - 1;
-			return (1);
-	}
-	return (0);
-}*/
