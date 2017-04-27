c104320993c000cd4c55b731375807b574da4b89
Lucas BOPP <lbopp@student.42.fr>
2017-04-27 12:54:28 +0200

    Parser : ispipe_sequence + test are actually ✅

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/24 12:10:28 by lbopp             #+#    #+#             */
-/*   Updated: 2017/04/26 16:38:42 by lbopp            ###   ########.fr       */
+/*   Updated: 2017/04/27 12:43:27 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,15 @@ int	isio_file(t_token *tok_lst, int nb_tok, int mv)
 		tmp--;
 	}
 	(void)nb_tok;
-	if (tok_lst->type == LESS && tok_lst->next->type == WORD)
+	if (tok_lst->type == LESS && tok_lst->next && tok_lst->next->type == WORD)
 			return (mv + 2);
-	else if (tok_lst->type == LESSAND && tok_lst->next->type == WORD)
+	else if (tok_lst->type == LESSAND && tok_lst->next && tok_lst->next->type == WORD)
 			return (mv + 2);
-	else if (tok_lst->type == GREAT && tok_lst->next->type == WORD)
+	else if (tok_lst->type == GREAT && tok_lst->next && tok_lst->next->type == WORD)
 		return (mv + 2);
-	else if (tok_lst->type == GREATAND && tok_lst->next->type == WORD)
+	else if (tok_lst->type == GREATAND && tok_lst->next && tok_lst->next->type == WORD)
 			return (mv + 2);
-	else if (tok_lst->type == DGREAT && tok_lst->next->type == WORD)
+	else if (tok_lst->type == DGREAT && tok_lst->next && tok_lst->next->type == WORD)
 			return (mv + 2);
 	return (0);
 }
@@ -57,7 +57,7 @@ int	isio_redirect(t_token *tok_lst, int nb_tok, int mv)
 			return (0);
 		tmp--;
 	}
-	if (nb_tok - tmp >= 2 && (tmp = isio_file(tok_lst, nb_tok, 0)))
+	if ((tmp = isio_file(tok_lst, nb_tok, 0)))
 		return (mv + tmp);
 	else if (tok_lst->type == IO_NUMBER &&
 			(mv = isio_file(tok_lst, nb_tok, 1)))
@@ -140,49 +140,96 @@ int	is_simplecmd(t_token *tok_lst, int nb_tok, int mv)
 			return (0);
 		tmp--;
 	}
+	tmp = mv;
 	if ((mv = iscmd_prefix(tok_lst, nb_tok, 0, 1)) && (mv = is_word(tok_lst, mv)) &&
 			(mv = iscmd_prefix(tok_lst, nb_tok, mv, 1)))
 	{
-		return (mv);
+		return (tmp + mv);
 	}
 	else if ((mv = iscmd_prefix(tok_lst, nb_tok, 0, 1)) && (mv = is_word(tok_lst, mv)))
 	{
-		return (mv);
+		return (tmp + mv);
 	}
 	else if ((mv = iscmd_prefix(tok_lst, nb_tok, 0, 1)))
-		return (mv);
-	else if (tok_lst->type == WORD && tok_lst->next &&
-			(mv = iscmd_prefix(tok_lst->next, nb_tok, 1, 1)))
-		return (mv + 1);
+	{
+		return (tmp + mv);
+	}
+	else if (tok_lst->type == WORD && (mv = iscmd_prefix(tok_lst, nb_tok, 1, 1)))
+	{
+		return (tmp + mv + 1);
+	}
 	else if (tok_lst->type == WORD)
-		return (1);
+	{
+		return (tmp + 1);
+	}
 	return (0);
 }
 
-int	main(void)
+int	ispipe(t_token *tok_lst, int mv)
 {
-	t_token	*tok_lst;
-	int	nb_tok;
-	int	mv;
+	int	tmp;
 
-	mv = 0;
-	nb_tok = 3;
-	tok_lst = (t_token*)ft_memalloc(sizeof(t_token));
-	tok_lst->i = 0;
-	tok_lst->type = IO_NUMBER;
-	tok_lst->content = ft_strdup("1");
-	tok_lst->next = (t_token*)ft_memalloc(sizeof(t_token));
-	tok_lst->next->i = 1;
-	tok_lst->next->type = GREAT;
-	tok_lst->next->content = ft_strdup(">");
-	tok_lst->next->next = (t_token*)ft_memalloc(sizeof(t_token));
-	tok_lst->next->next->i = 2;
-	tok_lst->next->next->type = WORD;
-	tok_lst->next->next->content = ft_strdup("lol");
-	tok_lst->next->next->next = NULL;/*(t_token*)ft_memalloc(sizeof(t_token));//NULL;
-	tok_lst->next->next->next->i = 3;
-	tok_lst->next->next->next->type = WORD;
-	tok_lst->next->next->next->content = ft_strdup("lol");
-	tok_lst->next->next->next->next = NULL;*/
-	printf("Result = %d\n", is_simplecmd(tok_lst, nb_tok, mv));
+	tmp = mv;
+	while (tmp > 0)
+	{
+		if (tok_lst->next)
+			tok_lst = tok_lst->next;
+		else
+			return (0);
+		tmp--;
+	}
+	if (tok_lst->type == PIPE)
+		return (mv + 1);
+	else
+		return (0);
+}
+
+int	isnewline(t_token *tok_lst, int mv)
+{
+	int	tmp;
+
+	tmp = mv;
+	while (tmp > 0)
+	{
+		if (tok_lst->next)
+			tok_lst = tok_lst->next;
+		else
+			return (0);
+		tmp--;
+	}
+	if (tok_lst->type == NEWLINE)
+		return (mv + 1);
+	else
+		return (0);
+}
+
+int	ispipe_sequence(t_token *tok_lst, int nb_tok, int mv, int mode)
+{
+	static int i = 0;
+	int	tmp;
+
+	tmp = mv;
+	if (mode == 1)
+		i = 0;
+	while (tmp > 0)
+	{
+		if (tok_lst->next)
+			tok_lst = tok_lst->next;
+		else
+			return (0);
+		tmp--;
+	}
+	i++;
+	if (nb_tok - mv >= 3 * i - (i - 1) && (mv = ispipe_sequence(tok_lst, nb_tok, 0, 2)) &&
+			(mv = ispipe(tok_lst, mv)) && (((tmp = isnewline(tok_lst, mv)) &&
+				(mv = is_simplecmd(tok_lst, nb_tok, tmp))) ||
+				(mv = is_simplecmd(tok_lst, nb_tok, mv))))
+	{
+		return (mv);
+	}
+	else if ((mv = is_simplecmd(tok_lst, nb_tok, 0)))
+	{
+		return (mv);
+	}
+	return (0);
 }
--- src/parser/testparser.c
+++ src/parser/testparser.c
@@ -6,13 +6,13 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/24 13:49:21 by lbopp             #+#    #+#             */
-/*   Updated: 2017/04/26 11:12:37 by lbopp            ###   ########.fr       */
+/*   Updated: 2017/04/27 12:51:01 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lsh.h"
 
-int	is_simplecmd(t_token *tok_lst, int nb_tok, int *max);
+int	ispipe_sequence(t_token *tok_lst, int nb_tok, int mv, int mode);
 
 int	test1(void)
 {
@@ -27,7 +27,7 @@ int	test1(void)
 	tok_lst->type = WORD;
 	tok_lst->content = ft_strdup("ls");
 	tok_lst->next = NULL;
-	if (is_simplecmd(tok_lst, nb_tok, &max) && max == nb_tok - 1)
+	if (nb_tok == ispipe_sequence(tok_lst, nb_tok, 0, 1))
 		return (1);
 	else
 		return (0);
@@ -50,8 +50,10 @@ int	test2(void)
 	tok_lst->next->type = GREAT;
 	tok_lst->next->content = ft_strdup(">");
 	tok_lst->next->next = NULL;
-	if (is_simplecmd(tok_lst, nb_tok, &max) && max == nb_tok - 1)
+	if (nb_tok == ispipe_sequence(tok_lst, nb_tok, 0, 1))
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
@@ -73,7 +75,7 @@ int	test3(void)
 	tok_lst->next->type = WORD;
 	tok_lst->next->content = ft_strdup("ls");
 	tok_lst->next->next = NULL;
-	if (is_simplecmd(tok_lst, nb_tok, &max) && max == nb_tok - 1)
+	if (nb_tok == ispipe_sequence(tok_lst, nb_tok, 0, 1))
 		return (1);
 	else
 		return (0);
@@ -84,7 +86,6 @@ int	test4(void)
 	int	max;
 	int	nb_tok;
 	t_token	*tok_lst;
-	int	test;
 
 	max = 0;
 	nb_tok = 3;
@@ -101,33 +102,203 @@ int	test4(void)
 	tok_lst->next->next->type = WORD;
 	tok_lst->next->next->content = ft_strdup("cat");
 	tok_lst->next->next->next = NULL;
-	if ((test = is_simplecmd(tok_lst, nb_tok, &max)) && max == nb_tok - 1)
+	if (nb_tok == ispipe_sequence(tok_lst, nb_tok, 0, 1))
 		return (1);
 	else
 	{
-		printf("TEST = %d et max = %d\n", test, max);
+		return (0);
+	}
+}
+
+int	test5(void)
+{	
+	int	nb_tok;
+	t_token	*tok_lst;
+
+	nb_tok = 3;
+	tok_lst= (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->i = 0;
+	tok_lst->type = WORD;
+	tok_lst->content = ft_strdup("ls");
+	tok_lst->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->i = 1;
+	tok_lst->next->type = PIPE;
+	tok_lst->next->content = ft_strdup("|");
+	tok_lst->next->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->next->i = 2;
+	tok_lst->next->next->type = WORD;
+	tok_lst->next->next->content = ft_strdup("cat");
+	tok_lst->next->next->next = NULL;
+	if (nb_tok == ispipe_sequence(tok_lst, nb_tok, 0, 1))
+		return (1);
+	else
+	{
+		return (0);
+	}
+}
+
+int	test6(void)
+{	
+	int	nb_tok;
+	t_token	*tok_lst;
+
+	nb_tok = 4;
+	tok_lst= (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->i = 0;
+	tok_lst->type = WORD;
+	tok_lst->content = ft_strdup("ls");
+	tok_lst->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->i = 1;
+	tok_lst->next->type = PIPE;
+	tok_lst->next->content = ft_strdup("|");
+	tok_lst->next->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->next->i = 2;
+	tok_lst->next->next->type = NEWLINE;
+	tok_lst->next->next->content = ft_strdup("\n");
+	tok_lst->next->next->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->next->next->i = 3;
+	tok_lst->next->next->next->type = WORD;
+	tok_lst->next->next->next->content = ft_strdup("cat");
+	tok_lst->next->next->next->next = NULL;
+	if (nb_tok == ispipe_sequence(tok_lst, nb_tok, 0, 1))
+		return (1);
+	else
+	{
+		return (0);
+	}
+}
+
+int	test7(void)
+{	
+	int	nb_tok;
+	t_token	*tok_lst;
+
+	nb_tok = 4;
+	tok_lst= (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->i = 0;
+	tok_lst->type = WORD;
+	tok_lst->content = ft_strdup("ls");
+	tok_lst->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->i = 1;
+	tok_lst->next->type = NEWLINE;
+	tok_lst->next->content = ft_strdup("\n");
+	tok_lst->next->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->next->i = 2;
+	tok_lst->next->next->type = PIPE;
+	tok_lst->next->next->content = ft_strdup("|");
+	tok_lst->next->next->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->next->next->i = 3;
+	tok_lst->next->next->next->type = WORD;
+	tok_lst->next->next->next->content = ft_strdup("cat");
+	tok_lst->next->next->next->next = NULL;
+	if (nb_tok == ispipe_sequence(tok_lst, nb_tok, 0, 1))
+		return (1);
+	else
+	{
+		return (0);
+	}
+}
+
+int	test8(void)
+{	
+	int	nb_tok;
+	t_token	*tok_lst;
+
+	nb_tok = 4;
+	tok_lst= (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->i = 0;
+	tok_lst->type = WORD;
+	tok_lst->content = ft_strdup("ls");
+	tok_lst->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->i = 1;
+	tok_lst->next->type = PIPE;
+	tok_lst->next->content = ft_strdup("|");
+	tok_lst->next->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->next->i = 2;
+	tok_lst->next->next->type = GREAT;
+	tok_lst->next->next->content = ft_strdup(">");
+	tok_lst->next->next->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->next->next->i = 3;
+	tok_lst->next->next->next->type = WORD;
+	tok_lst->next->next->next->content = ft_strdup("file");
+	tok_lst->next->next->next->next = NULL;
+	if (nb_tok == ispipe_sequence(tok_lst, nb_tok, 0, 1))
+		return (1);
+	else
+	{
+		return (0);
+	}
+}
+
+int	test9(void)
+{	
+	int	nb_tok;
+	t_token	*tok_lst;
+
+	nb_tok = 4;
+	tok_lst= (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->i = 0;
+	tok_lst->type = WORD;
+	tok_lst->content = ft_strdup("ls");
+	tok_lst->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->i = 1;
+	tok_lst->next->type = PIPE;
+	tok_lst->next->content = ft_strdup("|");
+	tok_lst->next->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->next->i = 2;
+	tok_lst->next->next->type = PIPE;
+	tok_lst->next->next->content = ft_strdup("|");
+	tok_lst->next->next->next = (t_token*)ft_memalloc(sizeof(t_token));
+	tok_lst->next->next->next->i = 3;
+	tok_lst->next->next->next->type = WORD;
+	tok_lst->next->next->next->content = ft_strdup("cat");
+	tok_lst->next->next->next->next = NULL;
+	if (nb_tok == ispipe_sequence(tok_lst, nb_tok, 0, 1))
+		return (1);
+	else
+	{
 		return (0);
 	}
 }
 
 void	launch_test_1to25(void)
 {
-	/*if (test1())
+	if (test1())
 		printf("\033[32m[OK] \033[0m[ls]\n");
 	else
 		printf("\033[31m[FAIL] \033[0m[ls]\n");
 	if (!test2())
 		printf("\033[32m[OK] \033[0m[ls >]\n");
 	else
-		printf("\033[31m[FAIL] \033[0m[ls >]\n");*/
+		printf("\033[31m[FAIL] \033[0m[ls >]\n");
 	if (test3())
 		printf("\033[32m[OK] \033[0m[> ls]\n");
 	else
 		printf("\033[31m[FAIL] \033[0m[> ls]\n");
-	/*if (test4())
+	if (test4())
 		printf("\033[32m[OK] \033[0m[> ls cat]\n");
 	else
-		printf("\033[31m[FAIL] \033[0m[> ls cat]\n");*/
+		printf("\033[31m[FAIL] \033[0m[> ls cat]\n");
+	if (test5())
+		printf("\033[32m[OK] \033[0m[ls | cat]\n");
+	else
+		printf("\033[31m[FAIL] \033[0m[ls | cat]\n");
+	if (test6())
+		printf("\033[32m[OK] \033[0m[ls | NEWLINE cat]\n");
+	else
+		printf("\033[31m[FAIL] \033[0m[ls | NEWLINE cat]\n");
+	if (!test7())
+		printf("\033[32m[OK] \033[0m[ls NEWLINE | cat]\n");
+	else
+		printf("\033[31m[FAIL] \033[0m[ls NEWLINE | cat]\n");
+	if (test8())
+		printf("\033[32m[OK] \033[0m[ls | > file]\n");
+	else
+		printf("\033[31m[FAIL] \033[0m[ls | > file]\n");
+	if (!test9())
+		printf("\033[32m[OK] \033[0m[ls | | file]\n");
+	else
+		printf("\033[31m[FAIL] \033[0m[ls | | file]\n");
 }
 
 int	main(void)
