219c2d316b4e6cb29cd5701b12ffc0902d3afb81
Lucas BOPP <lbopp@student.42.fr>
2017-10-02 16:10:49 +0200

    READ: without opt but need some test

--- read.c
+++ read.c
@@ -6,7 +6,7 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/02 14:08:53 by lbopp             #+#    #+#             */
-/*   Updated: 2017/10/02 14:16:17 by lbopp            ###   ########.fr       */
+/*   Updated: 2017/10/02 16:10:05 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,53 @@ char	*read_r_opt(char **cmd)
 	return (readding);
 }
 
+char	*read_without_opt(char **cmd)
+{
+	char	*readding;
+	char	buf[2];
+	int		backslash;
+
+	backslash = 0;
+	init_term();
+	readding = ft_strnew(0);
+	while (1)
+	{
+		ft_bzero(buf, 2);
+		read(0, buf, 2);
+		if (buf[0] == 10 && !backslash)
+			break ;
+		else if (buf[0] == 10 && backslash)
+		{
+			ft_putstr_fd("\n> ", 2);
+			backslash = 0;
+		}
+		else if (buf[0] == 127 && !buf[1])
+		{
+			if (readding)
+			{
+				tputs(tgetstr("le", NULL), 1, &put_my_char);
+				tputs(tgetstr("cd", NULL), 1, &put_my_char);
+				readding[ft_strlen(readding) - 1] = '\0';
+			}
+		}
+		else if (buf[0] == '\\')
+		{
+			ft_putchar(buf[0]);
+			if (!backslash)
+				backslash = 1;
+			else
+				readding = ft_stradd(readding, buf);
+		}
+		else if (ft_isprint(buf[0]) && !buf[1])
+		{
+			ft_putchar(buf[0]);
+			readding = ft_stradd(readding, buf);
+		}
+	}
+	default_term();
+	return (readding);
+}
+
 int		get_read_opt(char **cmd)
 {
 	int	size;
@@ -72,6 +119,26 @@ int		get_read_opt(char **cmd)
 
 int		valid_local_var(char **cmd)
 {
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (cmd[i])
+	{
+		while (cmd[i][j])
+		{
+			if (!ft_isalnum(cmd[i][j]) && cmd[i][j] != '_')
+			{
+				ft_putstr("shell: read: `");
+				ft_putstr(cmd[i]);
+				ft_putendl("': not a valid identifier");
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
 	return (1);
 }
 
@@ -83,11 +150,11 @@ void	read_put_in_var(char **cmd, char *readding)
 	i = 0;
 	if (!cmd[g_optind] || !valid_local_var(&cmd[g_optind]))
 		return ;
-	split = ft_strsplit(readding, ' ');
+	if (!(split = ft_strsplit(readding, ' ')))
+		return ;
 	printf("\n");
 	while (cmd[g_optind + 1])
 	{
-		if ()
 		printf("%s = [%s]\n", cmd[g_optind], split[i]);
 		g_optind++;
 		i++;
@@ -104,6 +171,7 @@ void	ft_read(char **cmd, char **env)
 	int		opt;
 	char	*readding;
 
+	readding = NULL;
 	opt = get_read_opt(cmd);
 	if (opt != 0 && opt != 'r')
 	{
@@ -112,6 +180,8 @@ void	ft_read(char **cmd, char **env)
 	}
 	if (opt == 'r')
 		readding = read_r_opt(cmd);
+	else
+		readding = read_without_opt(cmd);
 	read_put_in_var(cmd, readding);
 }
 
