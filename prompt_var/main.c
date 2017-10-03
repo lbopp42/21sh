f5501bc3a3bd6a1af876c6bad39a072e0c871c59
Lucas BOPP <lbopp@student.42.fr>
2017-10-03 17:06:26 +0200

    Prompt: All before uid

--- prompt_var/main.c
+++ prompt_var/main.c
@@ -6,7 +6,7 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/03 10:28:17 by lbopp             #+#    #+#             */
-/*   Updated: 2017/10/03 12:48:00 by lbopp            ###   ########.fr       */
+/*   Updated: 2017/10/03 16:51:51 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,72 @@ char	*get_ttyname(void)
 	return (name);
 }
 
+char	*get_time_prompt(int mode)
+{
+	char	*curr_time;
+	time_t		t;
+	struct tm	current;
+	int			hour;
+
+	time(&t);
+	current = *localtime(&t);
+	hour = current.tm_hour;
+	if (mode != 1 && hour > 12)
+		hour -= 12;
+	curr_time = ft_strdup(ft_itoa(hour));
+	curr_time = ft_stradd(curr_time, ":");
+	curr_time = ft_stradd(curr_time, ft_itoa(current.tm_min));
+	curr_time = ft_stradd(curr_time, ":");
+	curr_time = ft_stradd(curr_time, ft_itoa(current.tm_sec));
+	return (curr_time);
+}
+
+char	*get_time_prompt2(void)
+{
+	char	*curr_time;
+	time_t		t;
+	struct tm	current;
+	int			hour;
+
+	time(&t);
+	current = *localtime(&t);
+	hour = current.tm_hour;
+	if (hour > 12)
+		hour -= 12;
+	curr_time = ft_strdup(ft_itoa(hour));
+	curr_time = ft_stradd(curr_time, ":");
+	curr_time = ft_stradd(curr_time, ft_itoa(current.tm_min));
+	if (current.tm_hour < 12)
+		curr_time = ft_stradd(curr_time, " AM");
+	else
+		curr_time = ft_stradd(curr_time, " PM");
+	return (curr_time);
+}
+
+char	*get_username(void)
+{
+	char	*username;
+
+	if (!(username = ft_memalloc(sizeof(char) * 257)))
+		exit(EXIT_FAILURE);
+	getlogin_r(username, 256);
+	return (username);
+}
+
+char	*get_path(int mode)
+{
+	char	path[256];
+	char	*final_path;
+
+	getcwd(path, 256);
+	final_path = ft_strdup(path);
+	if (mode == 2)
+		final_path = ft_strrchr(final_path, '/');
+	if (final_path[0] && final_path[0] == '/')
+		final_path++;
+	return (final_path);
+}
+
 char	*get_prompt(char *code)
 {
 	int	i;
@@ -75,6 +141,24 @@ char	*get_prompt(char *code)
 			printf("%s\n", get_hostname_prompt(2));
 		else if (code[i] == '\\' && code[i + 1] && code[i + 1] == 'l')
 			printf("%s\n", get_ttyname());
+		else if (code[i] == '\\' && code[i + 1] && code[i + 1] == 's')
+			printf("%s\n", "42sh");
+		else if (code[i] == '\\' && code[i + 1] && code[i + 1] == 't')
+			printf("%s\n", get_time_prompt(1));
+		else if (code[i] == '\\' && code[i + 1] && code[i + 1] == 'T')
+			printf("%s\n", get_time_prompt(2));
+		else if (code[i] == '\\' && code[i + 1] && code[i + 1] == '@')
+			printf("%s\n", get_time_prompt2());
+		else if (code[i] == '\\' && code[i + 1] && code[i + 1] == 'u')
+			printf("%s\n", get_username());
+		else if (code[i] == '\\' && code[i + 1] && code[i + 1] == 'v')
+			printf("%s\n", "1.0");
+		else if (code[i] == '\\' && code[i + 1] && code[i + 1] == 'V')
+			printf("%s\n", "1.0.0");
+		else if (code[i] == '\\' && code[i + 1] && code[i + 1] == 'w')
+			printf("%s\n", get_path(1));
+		else if (code[i] == '\\' && code[i + 1] && code[i + 1] == 'W')
+			printf("%s\n", get_path(2));
 		i++;
 	}
 	return (NULL);
